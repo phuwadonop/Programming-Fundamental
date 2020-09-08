50c21cd5511b190819847273e590f8498eef74f3
phuwadonop <69117067+phuwadonop@users.noreply.github.com>
2020-09-08 17:41:21 +0700

    Add files via upload

---
+++ palindrome.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+
+using namespace std;
+
+int st_len (char* );
+
+int main() {
+
+    char text[100];
+    cin >> text;
+
+    int L = st_len(text);
+
+    if(L%2==1)
+    {
+        int mid = (L/2)+1;
+        char *textPtr;
+        textPtr = text;
+        int i=0;
+        while(i!=mid)
+        {   
+            int j = L - 1 -i;
+            if(textPtr[i] != textPtr[j])
+            {
+                cout << "Not Palindrome" << endl;
+                return 0;
+            }
+            i++;
+        }
+        cout << "Palindrome" << endl;
+    }
+    else if (L%2==0)
+    {
+        int mid = L/2;
+        char *textPtr;    
+        textPtr = text;
+        int i=0;
+        while(i <= mid)
+        {
+            int j = L - 1 -i;
+            if(textPtr[i] != textPtr[j])
+            {
+                cout << "Not Palindrome" << endl;
+                return 0;
+            }
+            i++;
+        }
+        cout << "Palindrome" << endl;
+    }
+    return 0;
+}
+int st_len (char x[100])
+{
+    int i = 0;
+    while(x[++i]!='\0');
+    return i;
+}
