8b68173373b806dc3f1a55c01f2f426d21db4f62
phuwadonop <69117067+phuwadonop@users.noreply.github.com>
2020-09-24 20:50:43 +0700

    Add files via upload

---
+++ PLANTATION.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std; 
+float dtPoint (int, int, int, int);
+char checkqc () ;
+
+int main () 
+{
+    int W;
+    cin >> W;
+
+    char ans[10]; char *ansPtr = ans;
+
+    for(int i = 0 ; i<W ; i++)
+    {
+        *(ansPtr+i) = checkqc();
+    }
+    for(int i = 0 ; i<W ; i++)
+    {
+       cout << *(ans + i) << "\n";
+    }
+ 
+
+}
+char checkqc () 
+{   
+    int N,R,D;
+    scanf("%d %d %d",&N,&R,&D);
+    long x[1000],y[1000];
+    for(int i=0 ; i<N ;i++)
+    {
+        scanf("%d %d",&x[i],&y[i]);
+    }
+    char ans;
+    for(int i=0 ; i<N ; i++)
+    {
+        for(int j=0 ; j<N ; j++)
+        {
+            if(i==j) continue;
+            if(dtPoint(x[i],y[i],x[j],y[j]) < (2*R) + D)
+                return ans = 'N';
+        }
+        i++;
+    }
+    return ans = 'Y';
+}
+float dtPoint (int x1, int y1, int x2, int y2)
+{
+    int D;
+
+    D = sqrt(pow((x1-x2),2)+pow((y1-y2),2));
+    
+    return D;
+}
